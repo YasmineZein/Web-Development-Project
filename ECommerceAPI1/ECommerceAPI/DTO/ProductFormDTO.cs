7c56495704e5b88c041a889730718eaa7436df09
unknown <yasminemohd56@gmail.com>
2025-05-06 21:10:57 +0300

    first commit

---
+++ ECommerceAPI1/ECommerceAPI/DTO/ProductFormDTO.cs
@@ -0,0 +1,14 @@
+﻿public class ProductFormDTO
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string SubCategory { get; set; }
+
+    public IFormFile? MainImage { get; set; }
+    public IFormFile? AdditionalImage1 { get; set; } 
+    public IFormFile? AdditionalImage2 { get; set; }
+    public IFormFile? AdditionalImage3 { get; set; } 
+}
